f32db04e75035cebeefcbd4b8034dde8f89a7ead
mateogarcialopez <mateogarcialopez3@gmail.com>
2019-11-16 02:35:28 -0500

    ultima

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "";		     // database password
+$dbname	= "modeloventas";    // database name
+
+
+
+?>
